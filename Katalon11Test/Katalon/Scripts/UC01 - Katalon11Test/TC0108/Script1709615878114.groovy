f7d602526782be703e85ededc8b7899c5438fa77
Justl3est <111554501+justbest2003@users.noreply.github.com>
2024-03-05 12:42:45 +0700

    Add files via upload

--- Katalon11Test/Katalon/Scripts/UC01 - Katalon11Test/TC0108/Script1709615878114.groovy
+++ Katalon11Test/Katalon/Scripts/UC01 - Katalon11Test/TC0108/Script1709615878114.groovy
@@ -29,7 +29,7 @@ WebUI.setText(findTestObject('Object Repository/Page_Customer Details Form/input
 
 WebUI.click(findTestObject('Page_Customer Details Form/input_Age_sub'))
 
-WebUI.verifyTextPresent('Customer Details', false)
+WebUI.verifyTextPresent('Customer Details Form', false)
 
 WebUI.takeScreenshot('C:\\xampp\\htdocs\\customer\\img\\TC08.png')
 
